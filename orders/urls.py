45f70ec77b29b13ef8427383c4421d2a30afa31f
Raj Undaviya <undaviyaraj2000@gmail.com>
2026-04-03 16:36:47 +0530

    Change in order's URL

--- orders/urls.py
+++ orders/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from .views import AdminSalesAnalyticsView, AdminTopProductsView, AdminUsersView, \
-    CartView, CartItemDetailView, WishlistDetailView, WishlistView, OrderView, OrderDetailView, \
-    AdminDashboardView, AdminOrderListView, AdminOrderUpdateStatusView
+from django.urls import path
+from .views import CartView, CartItemDetailView, WishlistDetailView, WishlistView, OrderView, OrderDetailView
 
 urlpatterns = [
     path('', OrderView.as_view(), name='orders'),
